c1039709c85071c352e8960145b27b67e39007de
Pietrek14 <dawidekpe@gmail.com>
2023-03-12 17:48:40 +0100

    fixed the floors

--- src/components/keybox.tsx
+++ src/components/keybox.tsx
@@ -10,12 +10,14 @@ export const Keybox = () => {
 		setKeys(Keys.keys);
 	}, []);
 
-	const firstFloorKeys = keys.filter(key => key.floor === 0);
-	const secondFloorKeys = keys.filter(key => key.floor === 1);
+	const basementKeys = keys.filter(key => key.floor === 0);
+	const firstFloorKeys = keys.filter(key => key.floor === 1);
+	const secondFloorKeys = keys.filter(key => key.floor === 2);
 
 	return (
 		<div className='flex flex-col gap-12 px-12'>
-			<Floor name='Parter' keys={firstFloorKeys} />
+			<Floor name='Parter / Piwnica' keys={basementKeys} />
+			<Floor name='Pierwsze piętro' keys={firstFloorKeys} />
 			<Floor name='Drugie piętro' keys={secondFloorKeys} />
 		</div>
 	)
